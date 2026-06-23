5debbd8762f95013fccb6de92aec1123a9231247
Johan Andres Mendoza Socha <johanmendoza2470@gmail.com>
2026-06-22 20:21:05 -0500

    amsas

--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,15 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
+
 
 
     # Le decimos al jefe que cuando alguien busque "eventos/", 
     # vaya y mire el archivo urls.py interno de la app 'event'
     path('eventos/', include('events.urls')),
     
-]
-=======
+
+
     path('foros/', include('forums.urls')),
 ]
->>>>>>> c54ecee7ed5c33ae2922d8ed66257e48848ea161
+
